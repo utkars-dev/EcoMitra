e451370c6dec0348e185c3aa6b6e7b9666f00db7
uktarsh <utkarsh2004mishra@gmail.com>
2025-09-26 22:16:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import Quiz from "./quiz";
-import Wastegame from "./waste";
-import Dash from "./dashboard";
-import Login from "./login";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Quiz from "./components/quiz";
+import Games from "./components/games";
+import Dash from "./components/dashboard";
+import Login from "./components/login";
+import Aboutus from "./components/About-us";
+import Game1 from "./components/game1";
+import Game2 from "./components/game2";
+import Game3 from "./components/game3";
+import Game4 from "./components/game4";
+import Game5 from "./components/game5";
+
+// Define all routes in one place
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path: "/quiz", element: <Quiz /> },
+  { path: "/game", element: <Games /> },
+  { path: "/dashboard", element: <Dash /> },
+  { path: "/aboutus", element: <Aboutus /> },
+
+  // Game routes (not in navbar)
+  { path: "/game1", element: <Game1 /> },
+  { path: "/game2", element: <Game2 /> },
+  { path: "/game3", element: <Game3 /> },
+  { path: "/game4", element: <Game4 /> },
+  { path: "/game5", element: <Game5 /> },
+];
 
 function App() {
   return (
     <div>
-      {/* <nav className="p-4 bg-gray-200 flex gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/quiz">Quiz</Link>
-      </nav> */}
-
+      {/* Route Definitions */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/waste" element={<Wastegame/>}/>
-        <Route path="/dashboard" element={<Dash/>}/>
+        {routes.map(route => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
       </Routes>
     </div>
   );
